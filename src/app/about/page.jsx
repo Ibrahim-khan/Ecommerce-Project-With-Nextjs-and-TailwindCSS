4d4a36f3779d672a676df9221f702c668640ba70
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-08-29 00:55:51 +0600

    BiosprayPlus-Website-With-Nextjs-and-TailwindCSS

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h1>This is about page</h1>
+      <h1>this is about page</h1>
     </div>
   )
 }
 
-export default page;
+export default page
