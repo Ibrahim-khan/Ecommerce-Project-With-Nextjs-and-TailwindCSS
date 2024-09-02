0451b38d090a1d4346a0fcaf5c722f647034a9a7
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-09-02 22:48:21 +0600

    BiosprayPlus-Website-With-Nextjs-and-TailwindCSS

--- src/components/NewProduct.jsx
+++ src/components/NewProduct.jsx
@@ -2,36 +2,6 @@ import React from 'react';
 import ProductCard from './ProductCard';
 
 
-const productsData = [
-  {
-    img: "/picture1.jpg",
-    title:"Bio Spray Plus",
-    desc: "Bio Spray Plus Colostrum Liquid",
-    rating: 5,
-    price: "45.00",
-  },
-  {
-    img: "/picture1.jpg",
-    title:"Bio Spray Plus",
-    desc: "Bio Spray Plus Colostrum Liquid",
-    rating: 4,
-    price: "35.00",
-  },
-  {
-    img: "/picture1.jpg",
-    title:"Bio Spray Plus",
-    desc: "Bio Spray Plus Colostrum Liquid",
-    rating: 3,
-    price: "25.00",
-  },
-  {
-    img: "/picture1.jpg",
-    title:"Bio Spray Plus",
-    desc: "Bio Spray Plus Colostrum Liquid",
-    rating: 2,
-    price: "15.00",
-  },
-]
 
 const NewProduct = () => {
   return (
@@ -39,23 +9,30 @@ const NewProduct = () => {
       <div className="container pt-16">
         <h2 className='font-medium text-2xl pb-4'>New Produts</h2>
       
-      <div className="grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 
-      lg:grid-cols-3 xl:grid-cols-4 gap-10 xl:gap-x-20 xl:gap-y-10">
-      
-      {productsData.map((item, index) => (
-        <ProductCard 
-          key= {index}
-          img={item.img}
-          title={item.title}
-          desc={item.desc}
-          rating={item.rating}
-          price={item.price} 
-        />
-      ))}
-      
+
+        <div class="mx-20 mt-40 md:mt-56 mb-10">
+            <div class="grid sm:grid-cols-2">
+                <div class="lg:size-80">
+                    <img src="./images/Images.png" alt="Food"/>
+                </div>
+                <div class="text-left ml-4 text-[#000000] tracking-[0.3]">
+                    <h1 class="font-semibold text-[18px] sm:text-[32px]">
+                        Take a taste, come join us. Life is so endlessly delicious
+                    </h1>
+                    <p class="my-5 text-[14px] sm:text-[18px]">
+                        In the artist's own experience, of course, art is fundamentally indefinable, unsayable; there is something sacred about its demands upon the soul, something inherently mysterious in the forms it takes
+                    </p>
+                    <button class="bg-[#FF006E] mt-2 px-3 py-2 font-semibold hover:bg-slate-700 text-white rounded-3xl">Download our App</button>
+                </div>
+            </div>
+        </div>
+
+
+     
+     
         </div>
       </div>
-    </div>
+ 
   )
 }
 
