8bb4b4ee2fa2036ef7eee9804b7ddb015d8c2ad8
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-09-12 02:21:35 +0600

    BiosprayPlus-Website-With-Nextjs-and-TailwindCSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,75 @@
 import React from 'react'
+import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <div className='bg-blackish text-gray-500 text-center py-4 pb-16 md:pb-4'>
-      Copyright @ Logo <br/>All Rights Reserved 2024.
+    <div className='bg-blackish text-gray-400 text-center py-5'>
+
+							
+        <div className="container">
+          <div className="flex w-full grid-cols-1 sm:grid-cols-3 space-x-10">
+            <div className='text-center'>
+              <p><b>Address:</b><br/>
+                <i>Shadhin Online Public Ltd.<br/>
+                Ayesha Shopping Complex,<br/>
+                85, Siddeshwari road,<br/>
+                Dhaka-1217.</i></p>
+            </div>
+            <div className='text-center'>
+              <p><b>Contact:</b><br/>
+                Email: <i>ibrahim2013khan@gmail.com</i><br/>
+                Phone: 01742 095 986.<br/>
+                What is app & Imo Connect: +880 1742 095 986</p>
+            </div>
+
+            <div className='flex space-x-5'>
+
+                <a href="https://www.facebook.com/BioSpray.Official.page" target="_blank">
+                <Image 
+                  className='header_top__icon_wrapper'
+                  src="/logo/fb.png" 
+                  alt="facebook page" 
+                  width={30} 
+                  height={30}
+                />
+                </a>
+                
+                <a href="https://www.youtube.com/@BioSprayPlusColostrumLiquid/featured" target="_blank">
+                <Image 
+                  className='header_top__icon_wrapper'
+                  src="/logo/youtube.png" 
+                  alt="Youtube Channel" 
+                  width={30}
+                  height={30}
+                />
+                </a>
+                
+                <a href="/">
+                <Image 
+                  className='header_top__icon_wrapper'
+                  src="/logo/linkedin.png" 
+                  alt="Linkedin" 
+                  width={30}
+                  height={30}
+                />
+                </a>
+                
+                <a href="http://www.abnutric.com/Web/products/ListOfProducts-bio.asp" target="_blank">
+                <Image 
+                  className='header_top__icon_wrapper'
+                  src="/logo/nutric.png" 
+                  alt="Nutric" 
+                  width={90}
+                  height={90}
+                />
+                </a>
+            </div>
+          </div>
+        </div>
+
+        <div className='bg-blackish text-gray-500 text-center py-4 pb-16 md:pb-4'>
+          Copyright @ Logo <br/>All Rights Reserved 2024.
+        </div>
     </div>
   )
 }
--- src/components/Testimony_Picture.jsx
+++ src/components/Testimony_Picture.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 const Testimony_Picture = () => {
   return (
-    <div>
-      <h1>Testimony Picture</h1>
-      <h2>testimony picture</h2>
+    <div className='w-full h-auto'>
+      <h1>Testimony</h1>
+      <div className='container pt-16 pb-16'>
+        <div className="flex grid-cols-4 col-span-4">
+          
+          <div className="w-full space-y-5">
+            <div className="flex space-x-10 justify-center">
+              <Image
+                className = "border border-gray-500 cursor-pointer hover:border-green-500"
+                src="/testimony/gangrine.jpg"
+                width={200}
+                height={200}
+                alt="dp"
+              />
+              <Image
+                className = "border border-gray-500 cursor-pointer hover:border-green-500"
+                src="/testimony/brest.jpg"
+                width={200}
+                height={200}
+                alt="dp"
+              />
+              <Image
+                className = "border border-gray-500 cursor-pointer hover:border-green-500"
+                src="/testimony/heart.jpg"
+                width={200}
+                height={200}
+                alt="dp"
+              />
+              <Image
+                className = "border border-gray-500 cursor-pointer hover:border-green-500"
+                src="/testimony/beauty.jpg"
+                width={200}
+                height={200}
+                alt="dp"
+              />
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
