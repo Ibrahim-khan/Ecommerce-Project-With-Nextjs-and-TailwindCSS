022112fc0131a98b60e226a98b7017baf14c17fd
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-10-04 23:37:17 +0600

    BiosprayPlus-Website-With-Nextjs-and-TailwindCSS

--- src/components/Certificate_Gallery.jsx
+++ src/components/Certificate_Gallery.jsx
@@ -6,75 +6,91 @@ const Certificate_Gallery = () => {
     <div className='w-full h-auto bg-gray-100'>
       <h1>Certificate & Gallery</h1>
       <div className='py-16'>
-        <div className="grid md:grid-cols-2">
+        <div className="grid md:grid-cols-2 space-y-16 md:space-x-5 md:px-5">
           
           <div className="w-full space-y-5">
             <div className="flex space-x-5 justify-center">
-              <Image
-                className = "border border-gray-500 cursor-pointer shadow-lg shadow-slate-300 rounded-lg transform transition duration-500 hover:scale-125"
-                src="/certificate/bd_nutrition_food.jpeg"
-                width={200}
-                height={200}
-                alt="dp"
-              />
-              <Image
-                className = "border border-gray-500 cursor-pointer shadow-lg shadow-slate-300 rounded-lg transform transition duration-500 hover:scale-125"
-                src="/certificate/bdscience.jpeg"
-                width={200}
-                height={200}
-                alt="dp"
-              />
+              <a href="/certificate/bd_nutrition_food.jpeg" target="_blank" rel="noopener noreferrer">
+                <Image
+                  className = "border border-gray-500 cursor-pointer shadow-lg shadow-slate-300 rounded-lg transform transition duration-500 hover:scale-110"
+                  src="/certificate/bd_nutrition_food.jpeg"
+                  width={200}
+                  height={200}
+                  alt="dp"
+                />
+              </a>
+              <a href="/certificate/bdscience.jpeg" target="_blank" rel="noopener noreferrer">
+                <Image
+                  className = "border border-gray-500 cursor-pointer shadow-lg shadow-slate-300 rounded-lg transform transition duration-500 hover:scale-110"
+                  src="/certificate/bdscience.jpeg"
+                  width={200}
+                  height={200}
+                  alt="dp"
+                />
+              </a>
             </div>
             <div className="flex space-x-5 justify-center">
-              <Image
-                className = "border border-gray-500 cursor-pointer shadow-lg shadow-slate-300 rounded-lg transform transition duration-500 hover:scale-125"
-                src="/certificate/abdul_malek.jpeg"
-                width={200}
-                height={200}
-                alt="dp"
-              />
-              <Image
-                className = "border border-gray-500 cursor-pointer shadow-lg shadow-slate-300 rounded-lg transform transition duration-500 hover:scale-125"
-                src="/certificate/halal.jpeg"
-                width={200}
-                height={200}
-                alt="dp"
-              />
+              <a href="/certificate/abdul_malek.jpeg" target="_blank" rel="noopener noreferrer">
+                <Image
+                  className = "border border-gray-500 cursor-pointer shadow-lg shadow-slate-300 rounded-lg transform transition duration-500 hover:scale-110"
+                  src="/certificate/abdul_malek.jpeg"
+                  width={200}
+                  height={200}
+                  alt="dp"
+                />
+              </a>
+              <a href="/certificate/halal.jpeg" target="_blank" rel="noopener noreferrer">
+                <Image
+                  className = "border border-gray-500 cursor-pointer shadow-lg shadow-slate-300 rounded-lg transform transition duration-500 hover:scale-110"
+                  src="/certificate/halal.jpeg"
+                  width={200}
+                  height={200}
+                  alt="dp"
+                />
+              </a>
             </div>
           </div>
 
           <div className="w-full space-y-5">
             <div className="flex space-x-5 justify-center">
-              <Image
-                className = "border border-gray-500 cursor-pointer shadow-lg shadow-slate-300 rounded-lg transform transition duration-500 hover:scale-125"
-                src="/gallery/training.jpeg"
-                width={200}
-                height={200}
-                alt="dp"
-              />
-              <Image
-                className = "border border-gray-500 cursor-pointer shadow-lg shadow-slate-300 rounded-lg transform transition duration-500 hover:scale-125"
-                src="/gallery/cfmeet.jpeg"
-                width={200}
-                height={200}
-                alt="dp"
-              />
+              <a href="/gallery/training.jpeg" target="_blank" rel="noopener noreferrer">
+                <Image
+                  className = "border border-gray-500 cursor-pointer shadow-lg shadow-slate-300 rounded-lg transform transition duration-500 hover:scale-110"
+                  src="/gallery/training.jpeg"
+                  width={200}
+                  height={200}
+                  alt="dp"
+                />
+              </a>
+              <a href="/gallery/cfmeet.jpeg" target="_blank" rel="noopener noreferrer">
+                <Image
+                  className = "border border-gray-500 cursor-pointer shadow-lg shadow-slate-300 rounded-lg transform transition duration-500 hover:scale-110"
+                  src="/gallery/cfmeet.jpeg"
+                  width={200}
+                  height={200}
+                  alt="dp"
+                />
+              </a>
             </div>
             <div className="flex space-x-5 justify-center">
-              <Image
-                className = "border border-gray-500 cursor-pointer shadow-lg shadow-slate-300 rounded-lg transform transition duration-500 hover:scale-125"
-                src="/gallery/thysonmeet.jpeg"
-                width={200}
-                height={200}
-                alt="dp"
-              />
-              <Image
-                className = "border border-gray-500 cursor-pointer shadow-lg shadow-slate-300 rounded-lg transform transition duration-500 hover:scale-125"
-                src="/gallery/seminar.jpeg"
-                width={200}
-                height={200}
-                alt="dp"
-              />
+              <a href="/gallery/thysonmeet.jpeg" target="_blank" rel="noopener noreferrer">
+                <Image
+                  className = "border border-gray-500 cursor-pointer shadow-lg shadow-slate-300 rounded-lg transform transition duration-500 hover:scale-110"
+                  src="/gallery/thysonmeet.jpeg"
+                  width={200}
+                  height={200}
+                  alt="dp"
+                />
+              </a>
+              <a href="/gallery/seminar.jpeg" target="_blank" rel="noopener noreferrer">
+                <Image
+                  className = "border border-gray-500 cursor-pointer shadow-lg shadow-slate-300 rounded-lg transform transition duration-500 hover:scale-110"
+                  src="/gallery/seminar.jpeg"
+                  width={200}
+                  height={200}
+                  alt="dp"
+                />
+              </a>
             </div>
           </div>
         </div>
